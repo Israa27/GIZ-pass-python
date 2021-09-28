4f4775fe7c584b8eca1787dc78abe47f63991f66
Israa27 <israa.adnan.1997@gmail.com>
2021-09-29 02:27:00 +0300

    upload solution task

--- python-pass.py
+++ python-pass.py
@@ -1,7 +1,17 @@
-
-
 class Solution:
-
     @staticmethod
     def longest_palindromic(s: str) -> str:
-        pass
+        def expandAroundCenter(left,right):
+            while(left>=0 and right<len(s) and s[left]==s[right]):
+                left-=1
+                right+=1
+            return s[left+1:right]
+        logestSub=""
+        for i in range(len(s)):
+            test=expandAroundCenter(i,i)
+            if len(test)>len(logestSub):
+               logestSub=test
+            test=expandAroundCenter(i,i+1)
+            if len(test)>len(logestSub):
+               logestSub=test
+        return logestSub
